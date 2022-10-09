faee5bc278d0da613ce8ca3d5dcad662558e4f6e
musabolug <musabolug@gmail.com>
2022-10-09 20:49:18 +0300

    Exercise9 is done

---
+++ Exercises/Exercise9.sql
@@ -0,0 +1,12 @@
+-- 1-) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city,country FROM city
+JOIN country ON city.country_id = country.country_id;
+-- 2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT customer.first_name,customer.last_name, payment.payment_id FROM customer
+JOIN payment ON payment.customer_id = customer.customer_id;
+
+-- 3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız
+
+SELECT rental.rental_id, customer.first_name,customer.last_name FROM customer
+JOIN rental ON rental.customer_id = customer.customer_id;
